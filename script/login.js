23b03bd67fa12564c9a6865dd1c32cb0fe741288
mdjobayerahamad <mdjobayerahamad26@gmail.com>
2026-02-19 18:18:14 +0600

    first commit 1st form section

---
+++ script/login.js
@@ -0,0 +1,10 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+   //1:get the mobile number input
+   const inputNumber = document.getElementById('number-input');
+   const contactNumber = inputNumber.value;
+   console.log(contactNumber);
+   //2:get the pin input
+   //3:match pin and mobile number
+   //3.1: true..>> alert>homepage
+   //3.2: false..> alert>return
+})
